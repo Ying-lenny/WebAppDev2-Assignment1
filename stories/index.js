da98faabf3cefcb081c70450d9ec838aac258dcf
Owen Lenihan <owenlenihan101@gmail.com>
2020-10-30 16:45:39 +0000

    Movie Details  page components

--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,8 @@ import MovieCard from "../src/components/movieCard";
 import FilterControls from "../src/components/filterControls";
 import MoviesHeader from "../src/components/headerMovieList";
 import MovieList from "../src/components/movieList";
-
+import MovieDetails from "../src/components/movieDetails";
+import MovieHeader from '../src/components/headerMovie'
 
 
 const sample = {
@@ -109,4 +110,10 @@ storiesOf("Home Page/FilterControls", module)
     return <MovieList movies={movies} />
 });
 
+storiesOf("Movie Details Page/MovieDetails", module).add("default", () => (
+  <MovieDetails movie={sample} />
+));
 
+storiesOf("Movie Details Page/MovieHeader", module).add("default", () => (
+  <MovieHeader movie={sample} />
+));
