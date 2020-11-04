54c311694d5bfed8b4811229928b167870e5b3e0
Owen Lenihan <owenlenihan101@gmail.com>
2020-11-04 12:56:12 +0000

    Full review page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
@@ -24,6 +26,7 @@ const App = () => {
           </li>
         </ul>
         <Switch>
+          <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
