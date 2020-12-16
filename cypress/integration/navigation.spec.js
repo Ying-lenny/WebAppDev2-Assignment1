a50996526f5042b394375c2c1426d8025d444708
Owen Lenihan <owenlenihan101@gmail.com>
2020-12-16 00:40:59 +0000

    Changed movie details function

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -83,27 +83,26 @@ describe("Navigation", () => {
   });
 });
 
-  // describe("From the Movie Details page ", () => {
-  //   beforeEach(() => {
-  //     cy.visit("/");
-  //     cy.visit(`/movies/${movieId}`);
-  //     cy.wait(2000)
-  //   });
+  describe("From the Movie Details page ", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get(".card").eq(1).find("img").click();
+    });
 
-  //   it("should change browser URL when show/hide reviews is clicked", () => {
-  //     cy.contains("Show Reviews").click();
-  //     cy.url().should("include", `/movies/${movieId}/reviews`);
-  //     cy.contains("Hide Reviews").click();
-  //     cy.url().should("not.include", `/movies/${movieId}/reviews`);
-  //   });
+    it("should change browser URL when show/hide reviews is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/reviews`);
+      cy.contains("Hide Reviews").click();
+      cy.url().should("not.include", `/reviews`);
+    });
 
-  //   it("should change browser URL when show/hide reviews is clicked", () => {
-  //       cy.contains("Show Reviews").click();
-  //       cy.url().should("include", `/movies/${movieId}/reviews`);
-  //       cy.get("tbody").find("a").eq(0).click();
-  //       cy.url().should("include", `/reviews/`);
-  //     });
-  //   });
+    it("should change browser URL when show/hide reviews is clicked", () => {
+        cy.contains("Show Reviews").click();
+        cy.url().should("include", `/reviews`);
+        cy.get("tbody").find("a").eq(0).click();
+        cy.url().should("include", `/reviews/`);
+      });
+    });
 
     describe("From the Favorites page", () => {
         beforeEach(() => {
